c2c872369836183ebcf6a8013303c7da3fe17ac9
Auto Updater <auto-updater@dashboard.local>
2026-07-26 09:05:09 +0800

    Auto update data: 09:00

--- data.js
+++ data.js
@@ -135,13 +135,6 @@ const dashboardData = [
 ];
 const historyData = [
   {
-    "timestamp": "2026-07-10T01:01:06.009Z",
-    "A": 54,
-    "A_amount": 7550,
-    "B": 23,
-    "B_amount": 3530
-  },
-  {
     "timestamp": "2026-07-10T07:01:13.532Z",
     "A": 54,
     "A_amount": 7550,
@@ -339,6 +332,13 @@ const historyData = [
   },
   {
     "timestamp": "2026-07-25T07:04:58.877Z",
+    "A": 51,
+    "A_amount": 7610,
+    "B": 34,
+    "B_amount": 4780
+  },
+  {
+    "timestamp": "2026-07-26T01:05:09.353Z",
     "A": 51,
     "A_amount": 7610,
     "B": 34,
